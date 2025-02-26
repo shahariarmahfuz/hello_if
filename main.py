bdb0b5c49db53c1e95376e5f4abaf5cbb804fb47
Shahariar Mahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2025-02-26 18:16:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -253,7 +253,7 @@ def ping():
     return jsonify({"status": "alive"})
 
 def keep_alive():
-    url = "https://hello-if-qn39.onrender.com"  # আপনার এপ্লিকেশনের URL দিয়ে পরিবর্তন করুন
+    url = "https://hello-if.onrender.com/ping"  # আপনার এপ্লিকেশনের URL দিয়ে পরিবর্তন করুন
     while True:
         time.sleep(300)  # 5 মিনিট পরপর পিং
         try:
